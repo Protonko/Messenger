998fb4aacd52cb7fbe9dca31b045d416e5bfecb1
Egor Ermolaev <probegrofanston@mail.ru>
2021-05-03 19:20:27 +0300

    client/update rem tests

--- client/src/utils/rem/__tests__/index.test.ts
+++ client/src/utils/rem/__tests__/index.test.ts
@@ -4,4 +4,8 @@ describe('rem', () => {
   it('should return 1rem', () => {
     expect(rem(16)).toBe('1rem')
   })
+
+  it('should return 0.5rem', () => {
+    expect(rem(16, 32)).toBe('0.5rem')
+  })
 })
