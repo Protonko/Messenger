33728c43fc7ede2c7bd8a38747bd726a01287595
Egor Ermolaev <probegrofanston@mail.ru>
2021-06-14 14:12:19 +0300

    client/create throttle func

---
+++ client/src/utils/throttle/index.ts
@@ -0,0 +1,21 @@
+import debounce from 'lodash.debounce'
+
+export interface IOptions {
+  leading?: boolean
+  trailing?: boolean
+}
+
+const defaultOptions: IOptions = {
+  leading: true,
+  trailing: true,
+}
+
+export const throttle = (func: (args?: any) => void, wait: number, options = defaultOptions) => {
+  return debounce(func, wait, {
+    leading: options.leading,
+    trailing: options.trailing,
+    maxWait: wait,
+  })
+}
+
+export default throttle
