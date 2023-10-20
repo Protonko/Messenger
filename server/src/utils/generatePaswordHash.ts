fe8b85246799dbbba3295c5c0ff58bb89f7caf66
Егор Ермолаев <probegrofanston@mail.ru>
2023-10-20 22:33:22 +0400

    Update dependencies

--- server/src/utils/generatePaswordHash.ts
+++ server/src/utils/generatePaswordHash.ts
@@ -2,7 +2,7 @@ import {hash} from 'bcrypt'
 
 export const generatePasswordHash = (password: string): Promise<string> => {
   return new Promise(async (resolve, reject) => {
-    await hash(password, 10, (error: Error, hash: string) => {
+    await hash(password, 10, (error: Error | undefined, hash: string) => {
       if (error) {
         return reject(error)
       }
